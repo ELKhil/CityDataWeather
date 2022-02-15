27ac6dd6d5317abc2af7f1c680a3b8b2e142894e
Mohamed Saad El Khil <m.s.elkhil@gmail.com>
2022-02-15 16:10:51 +0100

    version 02

--- script.js
+++ script.js
@@ -1,8 +1,78 @@
+
+const URLMETEO = "http://api.openweathermap.org/data/2.5/";
+const APPID = "668484f6dbe8b8f6daf28d10bf49bc08"
 const today = document.querySelector(".today");
 const tomorrow = document.querySelector(".tomorrow");
 const totomorrow = document.querySelector(".totomorrow");
 
-let callBackgetSuccess = function(data){
+
+
+//informations météo de la ville
+// function buttonClickGet(){
+   
+//     let url="https://api.openweathermap.org/data/2.5/weather?q="+document.getElementById("nomVille").value+"&units=metric&appid=668484f6dbe8b8f6daf28d10bf49bc08"
+//     $.get(url, callBackgetSuccess).done(function(){
+
+//          })
+//          .fail(function(){
+//             alert("Veuillez vérifier le nom de la ville merci ...")
+//             })
+//         .always(function(){
+
+//          });
+  
+// }    
+
+
+// let callBackgetSuccess = function(data){
+//     console.log("donnes api_1", data)
+//     console.log(data)
+//     const temp = document.getElementById("temp")
+//     temp.innerHTML = "La température actuelle est de : "+data.main.temp+" C°";
+//     const tempMin = document.getElementById("tempMin")
+//     tempMin.innerHTML = "La température minimum est de : "+data.main.temp_min+" C°";
+//     const tempMax = document.getElementById("tempMax")
+//     tempMax.innerHTML = "La température maximum est de : "+data.main.temp_max+" C°";
+//     const humid = document.getElementById("hum")
+//     humid.innerHTML = "Le taux d'humidité est de : "+data.main.humidity+" %";
+//     const vent = document.getElementById("vent")
+//     vent.innerHTML = "La vitesse du vent est de : "+((data.wind.speed/1000)/(1/3600))+" km/h";
+//     const pression = document.getElementById("press")
+//     pression.innerHTML = "La valeur de pression est de : "+data.main.pressure+" hPa";
+//     const city = document.querySelector(".cityName")
+//     city.innerHTML= "⇩ "+document.getElementById("nomVille").value+" ⇩";
+//     const map = document.querySelector(".map")
+//     map.src="https://www.google.com/maps?q="+document.getElementById("nomVille").value+"&z=5&output=embed"
+  
+// }
+
+//Méthode Ajax
+
+function request(callback, ville){
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        // console.log(xhr.readyState + " | " + xhr.status);
+        if(xhr.readyState == 4 && xhr.status == 200)
+        {
+            // console.log(JSON.parse(xhr.responseText));
+            let myJson = JSON.parse(xhr.responseText);
+            // console.log(myJson.name + " : " + myJson.main.temp)
+            callback(myJson)
+            //afficherMeteo(myJson)
+        }
+        console.log("golilili"+xhr.status);
+        if (xhr.status == 404){
+            alert("Erreur nom ville");
+        }
+    }
+    
+    xhr.open("GET", `${URLMETEO}weather?q=${ville}&units=metric&lang=fr&appid=${APPID}`, true);
+    xhr.send()
+}
+
+
+function afficherMeteo(data){
     console.log("donnes api_1", data)
     console.log(data)
     const temp = document.getElementById("temp")
@@ -21,8 +91,49 @@ let callBackgetSuccess = function(data){
     city.innerHTML= "⇩ "+document.getElementById("nomVille").value+" ⇩";
     const map = document.querySelector(".map")
     map.src="https://www.google.com/maps?q="+document.getElementById("nomVille").value+"&z=5&output=embed"
-  
 }
+
+function buttonClickGet(){
+    let ville = document.getElementById("nomVille").value;
+    request(afficherMeteo, ville)
+    // requestFetch(afficherMeteo, ville)
+}
+
+//Méthode Fetch
+// function requestFetch(callback, ville){
+        
+//     fetch(`${URLMETEO}weather?q=${ville}&units=metric&lang=fr&appid=${APPID}`)
+
+   
+//     .then( response => {// .then ( function(response){// })
+//         console.log(response); 
+    
+//         if(response.ok)
+//         {
+//             return response.json();
+//         }
+//         else {
+//             alert("erreur") ;
+//         }
+//     }
+//     ).then( data => {
+//         console.log(data);
+//         callback(data);
+//     }
+//     )
+//     .catch(err => 
+//         console.error = "Erreur de type : " + err
+//     )
+// }
+
+
+
+
+
+
+
+
+
 //précipitations d'aujourd'hui
 let callBack2getSuccess = function(data2){
 
@@ -30,7 +141,6 @@ let callBack2getSuccess = function(data2){
             today.classList.toggle("show");
             console.log("donnes apis_2", data2)
             console.log(data2)
-
 
 
             var city = document.createElement('p')
@@ -89,21 +199,7 @@ let callBack2getSuccess = function(data2){
    
      
 
-//informations météo de la ville
-function buttonClickGet(){
-   
-    let url="https://api.openweathermap.org/data/2.5/weather?q="+document.getElementById("nomVille").value+"&units=metric&appid=668484f6dbe8b8f6daf28d10bf49bc08"
-    $.get(url, callBackgetSuccess).done(function(){
 
-         })
-         .fail(function(){
-            alert("Veuillez vérifier le nom de la ville merci ...")
-            })
-        .always(function(){
-
-         });
-  
-}    
 //précipitations d'aujourd'hui
 function button2ClickGet(){
     const url="https://api.openweathermap.org/data/2.5/forecast?q="+document.getElementById("nomVille").value+"&appid=668484f6dbe8b8f6daf28d10bf49bc08"
@@ -146,11 +242,9 @@ function button3ClickGet(){
                 console.log("le jour d'auhordhui "+d.getDay());
                 var dtt =new Date(data2.list[i].dt*1000)
                 var condition =false;
-                    if(d.getDay()==6){
-                        if(d.getDay()+1 == 0)
+                    if((d.getDay()=== 6) && (d.getDay()+1 === 0) ){
                         condition=true;
-                    }else{
-                    if(dtt.getDay() === d.getDay()+1){
+                    }else if (dtt.getDay() === d.getDay()+1){
                         condition=true;
                     }
          if(condition){
@@ -190,7 +284,7 @@ function button3ClickGet(){
             document.querySelector(".tomorrow").appendChild(divtest)
         }
     }
-}
+
     
     
 
@@ -225,54 +319,58 @@ function button3ClickGet(){
 
         for(var i=0;i<40;i++){
    
-               console.log("le jour d'auhordhui "+d.getDay());
-               var dtt =new Date(data2.list[i].dt*1000)
-               var condition =false;
-            if(d.getDay()==5){
-                if(d.getDay()+2== 0)
+            console.log("le jour d'auhordhui "+d.getDay());
+            var dtt =new Date(data2.list[i].dt*1000)
+            var condition =false;
+            console.log("aujourd'hui "+ d.getDay());
+            console.log("jour de la liste "+ dtt.getDay());
+
+            
+            if((d.getDay()===5) && (dtt.getDay() === 0)){
                 condition=true;
-            }else{
-            if(dtt.getDay() === d.getDay()+2){
+            }else if (dtt.getDay() === d.getDay()+2){
                 condition=true;
             }
-          if(condition){
 
-            var divtest= document.createElement('div');
-            var date3H =document.createElement('p');
-            var textdate = document.createTextNode(data2.list[i].dt_txt);
-            date3H.appendChild(textdate);
-            divtest.appendChild(date3H);
 
-            if (Object.keys(data2.list[i]).includes('rain')==false){
+            if(condition){
+
+                        var divtest= document.createElement('div');
+                        var date3H =document.createElement('p');
+                        var textdate = document.createTextNode(data2.list[i].dt_txt);
+                        date3H.appendChild(textdate);
+                        divtest.appendChild(date3H);
+
+                        if (Object.keys(data2.list[i]).includes('rain')==false){
+                            
+                            var pluie =document.createElement('p');
+                            textPluie = document.createTextNode("Absence des donnés");
+                            pluie.appendChild(textPluie)
+                            divtest.appendChild(pluie)
+
+                            var icon =document.createElement('img');
+                            icon.src = "absenceDonner.png"
+                            
+                            divtest.appendChild(icon)
+                    
+                        }else{
+                        
+                            var pluie =document.createElement('p');
+                            textPluie = document.createTextNode(data2.list[i].rain['3h']+" mm");
+                            pluie.appendChild(textPluie)
+                            divtest.appendChild(pluie)
+
+                            var icon =document.createElement('img');
+                            const iconcode =data2.list[i].weather[0].icon;
+                            icon.src = "http://openweathermap.org/img/w/"+iconcode+".png"
+                            
+                            divtest.appendChild(icon)
+                        }
                 
-                var pluie =document.createElement('p');
-                textPluie = document.createTextNode("Absence des donnés");
-                pluie.appendChild(textPluie)
-                divtest.appendChild(pluie)
-
-                var icon =document.createElement('img');
-                icon.src = "absenceDonner.png"
-                
-                divtest.appendChild(icon)
-        
-            }else{
-              
-                var pluie =document.createElement('p');
-                textPluie = document.createTextNode(data2.list[i].rain['3h']+" mm");
-                pluie.appendChild(textPluie)
-                divtest.appendChild(pluie)
-
-                var icon =document.createElement('img');
-                const iconcode =data2.list[i].weather[0].icon;
-                icon.src = "http://openweathermap.org/img/w/"+iconcode+".png"
-                
-                divtest.appendChild(icon)
-            }
-      
-            document.querySelector(".totomorrow").appendChild(divtest)
-        }
+                        document.querySelector(".totomorrow").appendChild(divtest)
+                    }
     }
-    }
+    
         
     }).done(function(){
 
@@ -321,12 +419,12 @@ function buttonBlague(){
 }   
 //Creer une date 
 function afficheHeure(){
-    const d =new Date();
-    const heure = d.getHours();
-    const minutes = d.getMinutes();
-    const secondes = d.getSeconds();
-    var heurMinutes = document.createElement('span');
-    var heureText= document.createTextNode("L'heure locale   "+heure +"h : "+minutes+"min  : "+secondes+" s  ");
+    let d =new Date();
+    let heure = d.getHours();
+    let minutes = d.getMinutes();
+    let secondes = d.getSeconds();
+    let heurMinutes = document.createElement('span');
+    let heureText= document.createTextNode("L'heure locale   "+(heure<10 ? `0${heure}` : `${heure}` +"h : ")+(minutes<10 ? `0${minutes}` : `${minutes}`+" min  : ")+(secondes<10 ?  `0${secondes}` : `${secondes}` +" s"));
     heurMinutes.appendChild(heureText);
     document.querySelector("header").appendChild(heurMinutes);
 }
